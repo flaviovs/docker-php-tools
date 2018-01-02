7422c9c2103902351e0a2e3aa63c6d4811bd2b12
Flávio Veloso <flaviovs@magnux.com>
2018-01-02 11:05:35 -0800

    Fix push.sh not working properly

--- push.sh
+++ push.sh
@@ -2,16 +2,15 @@ set -e
 
 sh build.sh
 
-for V in 5.6 7.0 7.1 7.2; do
+for V in latest 5.6 7.0 7.1 7.2; do
 	TAG="flaviovs/php-tools:$V"
 	DRUPAL_TAG="flaviovs/drupal-tools:$V"
 	WORDPRESS_TAG="flaviovs/wordpress-tools:$V"
 
 	docker push "$TAG"
-	docker push "$DRUPAL_TAG"
+	if [ "$V" != "7.2" ]; then
+		docker push "$DRUPAL_TAG"
+	fi
 	docker push "$WORDPRESS_TAG"
 done
 
-docker tag "$TAG" flaviovs/php-tools:latest
-docker tag "$DRUPAL_TAG" flaviovs/drupal-tools:latest
-docker tag "$WORDPRESS_TAG" flaviovs/wordpress-tools:latest
