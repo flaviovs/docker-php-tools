bf6672939e3e70f5bae45d5cae1b244da3ee325e
Flávio Veloso <flaviovs@magnux.com>
2019-06-10 12:13:53 -0700

    Add 7.3 to push.sh

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@ set -e
 
 sh build.sh
 
-for V in latest 5.6 7.0 7.1 7.2; do
+for V in latest 5.6 7.0 7.1 7.2 7.3; do
 	TAG="flaviovs/php-tools:$V"
 	DRUPAL_TAG="flaviovs/drupal-tools:$V"
 	WORDPRESS_TAG="flaviovs/wordpress-tools:$V"
